5aea6f0d405778f288545fe79d6c1de27c805f45
Kkhushii <khsuhi2002y@gmail.com>
2021-05-29 05:20:52 +0530

    new UI

--- app/src/main/java/hackOn2021/cheerymate/MainActivity.java
+++ app/src/main/java/hackOn2021/cheerymate/MainActivity.java
@@ -3,12 +3,32 @@ package hackOn2021.cheerymate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView AppName;
+    private Button signin;
+    private Button login;
+    EditText username,pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppName = findViewById(R.id.Appname);
+
+        username = findViewById(R.id.username);
+        pass = findViewById(R.id.password);
+        final String user = username.getText().toString().trim();
+        final String password = pass.getText().toString().trim();
+
+        signin = findViewById(R.id.gsignin);
+        login = findViewById(R.id.login);
+
+
     }
 }
