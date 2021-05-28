a8332e713374111be4361a6e178820c1c22cc9cb
Tothagata Bhattacharjee <tothagata.bhattacharjee@gmail.com>
2021-05-28 22:31:46 +0530

    Initial Commit

---
+++ app/src/main/java/hackOn2021/cheerymate/MainActivity.java
@@ -0,0 +1,14 @@
+package hackOn2021.cheerymate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
